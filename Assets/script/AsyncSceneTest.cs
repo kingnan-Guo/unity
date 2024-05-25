23210a0a53696cf53d6c493c0e5863f2c9f2fb04
kingnan <373734199@qq.com>
2024-05-25 23:37:42 +0800

    异步懒加载 场景

--- Assets/script/AsyncSceneTest.cs
+++ Assets/script/AsyncSceneTest.cs
@@ -7,6 +7,7 @@ public class AsyncSceneTest : MonoBehaviour
 {
     // 声明一个异步操作变量
     AsyncOperation operation;// 声明一个异步操作变量
+    float time = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,12 +23,17 @@ public class AsyncSceneTest : MonoBehaviour
     {
         // 创建一个异步操作，用于加载场景
         operation = SceneManager.LoadSceneAsync("myScenes");
+        operation.allowSceneActivation = false; // 禁止场景自动激活
         // yield return new WaitForSeconds(5); // 等待5秒
         yield return operation; // 等待异步操作完成
     }
     // Update is called once per frame
     void Update()
     {
-        
+        time+= Time.deltaTime;
+        if (time > 5){
+            operation.allowSceneActivation = true; // 允许场景自动激活
+        }
+        Debug.Log(operation.progress);
     }
 }
