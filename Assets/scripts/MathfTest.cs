6214244a9838229b9c2b59cbf55cf3bd8ef2b617
kingnan <373734199@qq.com>
2024-05-31 00:30:08 +0800

    Update MathfTest.cs

--- Assets/scripts/MathfTest.cs
+++ Assets/scripts/MathfTest.cs
@@ -12,6 +12,30 @@ public class MathfTest : MonoBehaviour
         // 度 转 弧度
         float degree= r * Mathf.Rad2Deg;
         Debug.Log("弧度：" + r + "度：" + degree);
+        r = degree * Mathf.Deg2Rad;
+        Debug.Log("弧度：" + r + "度：" + degree);
+        // 线性插值
+        float a = Mathf.Lerp(0, 100, 0.5f);
+        Debug.Log("插值：" + a);
+
+        // 反三角函数 求角度
+        float b = Mathf.Acos(0.5f);
+        Debug.Log("反三角函数：" + b);
+        float c = Mathf.Sin(b);
+        Debug.Log("反三角函数：" + c);
+        float d = Mathf.Cos(b);
+        Debug.Log("反三角函数：" + d);
+
+        float x1 =1;
+        float x2 = 2;
+        float y1 = 1;
+
+        float y2 = 2;
+        r = Mathf.Atan2(y2 - y1, x2 - x1);
+        Debug.Log("两点间角度：" + r);
+
+        degree = r * Mathf.Rad2Deg;
+        Debug.Log("两点间角度：" + degree);
     }
 
     // Update is called once per frame
